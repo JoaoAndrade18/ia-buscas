2f09f37f7750a7a0973fb141275332e86e4620fb
JoaoAndrade18 <batistajoaoguns@alu.ufc.br>
2023-09-18 12:06:05 -0300

    new

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import espacoDeEstados.*;
 import estrategiasDeBusca.cega.*;
-// import estrategiasDeBusca.heuristica.*;
+import estrategiasDeBusca.heuristica.*;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,21 +10,21 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// Variaveis de configuracao do puzzle	
-		Scanner input = new Scanner(System.in);
+		// Scanner input = new Scanner(System.in);
 		char[] cfgIni;
 		char[] cfgEnd;
 
 		// Gerador de numeros aleatorios
-		Random random = new Random();		
+		// Random random = new Random();		
 		
-		int numeroAleatorio3 = random.nextInt(3) +1;
+		// int numeroAleatorio3 = random.nextInt(3) +1;
 
-		cfgIni = new char[]{'2','3',' ','7','4','1','5','8','6'}; 
-		cfgEnd = new char[]{'1','2','3','4','5','6','7','8', ' '};
+		cfgIni = new char[]{'1','2','3','4','5','6','7',' ','8'}; 
+		cfgEnd = new char[]{'1','2','3','4','5','6','7','8',' '};
 
 
 		Puzzle8 puzzleInicial = new Puzzle8();
-		puzzleInicial.setEstado(cfgIni);
+		puzzleInicial.setEstado( cfgIni );
 		puzzleInicial.setCusto(0);
 		puzzleInicial.setAvaliacao( puzzleInicial.heuristica(Puzzle8.TABULEIRO_ORGANIZADO) );
 			
@@ -36,23 +36,30 @@ public class Main {
 		
 		// Modelo de busca a ser utilizado para resolver o problema
 
-        BuscaCega busca = null;
+        // BuscaCega busca = null;
 
-        if (numeroAleatorio3 == 1) {
-			System.out.println("Busca em Largura selecionada");
-            busca = new BuscaEmLargura();
-        } else if (numeroAleatorio3 == 2) {
-			System.out.println("Busca em Profundidade selecionada"); // Out of memory
-            busca = new BuscaEmProfundidade();
-        } else {
-			System.out.println("Busca em Profundidade Limitada selecionada, informe o limite:"); // acima de 13 para funcionar
-			int limite = input.nextInt();
-			System.out.println("Limite informado: " + limite);
-            busca = new BuscaEmProfundidadeLimitada(null, null, limite);
-			input.close();
-        } 
-		
-		
+        // if (numeroAleatorio3 == 1) {
+		// 	System.out.println("Busca em Largura selecionada");
+        //     busca = new BuscaEmLargura();
+        // } else if (numeroAleatorio3 == 2) {
+		// 	System.out.println("Busca em Profundidade selecionada"); // Out of memory
+        //     busca = new BuscaEmProfundidade();
+        // } else {
+		// 	System.out.println("Busca em Profundidade Limitada selecionada, informe o limite:"); // acima de 13 para funcionar
+		// 	int limite = input.nextInt();
+		// 	System.out.println("Limite informado: " + limite);
+        //     busca = new BuscaEmProfundidadeLimitada(null, null, limite);
+		// 	input.close();
+        // } 
+
+		// BuscaCega busca = new BuscaEmLargura();
+		BuscaCega busca = new BuscaEmProfundidade();
+		// BuscaCega busca = new BuscaEmProfundidadeLimitada(null, null, 14);
+
+		// BuscaInformada busca = new AStar();
+		// BuscaInformada busca = new HillClimbing();
+		// BuscaInformada busca = new BestFirst();
+
 		busca.setInicio(puzzleInicial);
 		busca.setObjetivo(puzzleFinal);
 		busca.buscar();
